3f9b750af8d925c091342875161d8d366ea03391
nicstreet <street.nic@gmail.com>
2016-01-17 22:52:54 +0000

    Include new Keyboard NRS67
    Include new Keyboard NRS67
    
    2 Layers with working Mousekeys

---
+++ keyboard/NRS67/keymap_plain - Copy.c.backup
@@ -0,0 +1,16 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	KEYMAP (ESC, 1,   2,   3,   4,   5,   6, \
+			7, 	8,   9,   0,   MINS,EQL,BSPC, \
+			TAB, Q,   W,   E,   R,   T,   Y, \
+			U,   I,   O,   P,   LBRC,RBRC,DEL, \			
+			FN0 ,A,   S,   D,   F,   G,   H, \
+			J,   K,   L,   SCLN,QUOT, ENT, \			
+			LSFT,Z,   X,   C,   V,   B,   N, \
+			M,   COMM,DOT, SLSH,HOME,UP,END, \			
+			LCTL,LGUI,LALT,CAPS,PGDN,SPC, \
+			SPC, PGUP,LSFT,LEFT,DOWN,RGHT),
+};
+
+const uint16_t PROGMEM fn_actions[] = {};
---
+++ keyboard/NRS67/keymap_plain.c
@@ -0,0 +1,67 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Standard NRS67 Keymap */
+	KEYMAP (
+			ESC, 1,   2,   3,   4,   5,   6, \
+			7,   8,   9,   0,   MINS,EQL, BSPC, \
+			TAB, Q,   W,   E,   R,   T,   Y, \
+			U,   I,   O,   P,   LBRC,RBRC,DEL, \
+			FN0, A,   S,   D,   F,   G,   H, \
+			J,   K,   L,   SCLN,QUOT,ENT, \
+			LSFT,BSLS,Z,   X,   C,   V,   B, \
+			N,   M,   COMM,DOT, RSFT,UP,  CAPS, \
+			LCTL,LGUI,LALT,PGDN,PGUP,SPC, \
+			SPC, HOME,END,LEFT,DOWN,RGHT),
+			
+/* FN0 : NRS67 FN0'd */
+    KEYMAP (
+			TRNS,F1,  F2,  F3,  F4,  F5,  F6, \
+			F7,  F8,  F9,  F10, F11, F12, TRNS, \
+			TRNS,FN1, MS_U,TRNS,WH_U,TRNS,TRNS, \
+			TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+			FN0 ,MS_L,MS_D,MS_R,WH_D,TRNS,TRNS, \
+			TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+			TRNS,TRNS,TRNS,CALC,TRNS,TRNS,TRNS, \
+			TRNS,TRNS,TRNS,TRNS,TRNS,VOLU,MUTE, \
+			TRNS,TRNS,TRNS,TRNS,TRNS,BTN1, \
+			BTN2,TRNS,TRNS,TRNS,VOLD,TRNS),
+};
+
+
+enum macro_id {
+    HELLO,
+    VOLUP,
+    ALT_TAB,
+};
+
+
+const uint16_t PROGMEM fn_actions[] = {
+    /* Poker Layout */
+    [0] = ACTION_LAYER_MOMENTARY(1), 			// FN0 move to Fn'd
+	[1] = ACTION_MODS_KEY(MOD_LSFT, KC_SLSH),
+	[30] = ACTION_MACRO(HELLO),					// Add FN31 or FN32 to keymap if needed
+	[31] = ACTION_MODS_KEY(MOD_LCTL + MOD_LSFT, KC_ESC),
+};
+
+/*
+ * Macro definition
+ */
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    switch (id) {
+        case HELLO:
+            return (record->event.pressed ?
+                    MACRO( I(0), T(H), T(E), T(L), T(L), W(255), T(O), END ) :
+                    MACRO_NONE );
+        case VOLUP:
+            return (record->event.pressed ?
+                    MACRO( D(VOLU), U(VOLU), END ) :
+                    MACRO_NONE );
+        case ALT_TAB:
+            return (record->event.pressed ?
+                    MACRO( D(LALT), D(TAB), END ) :
+                    MACRO( U(TAB), END ));
+    }
+    return MACRO_NONE;
+}
